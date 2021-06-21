49f7c0aa801a6c80550e893e72386cf73bfabf48
Aaron E <aarone@vmware.com>
2021-06-21 20:38:06 +1000

    updtes

--- vra8/main.tf
+++ vra8/main.tf
@@ -1,35 +1,4 @@
-provider "vra" {
+provider vra {
   url = var.vra_url
-  refresh_token = vra_refresh_token
-  insecure = true
-}
-
-resource "vra_blueprint" "this" {
-   name = var.blueprint_name
-   description = "Test blueprint from terraform"
-   project_id = vra_project.this.id
-    
-   content = <<-EOT
-      name: Win2K16
-      version: 1
-      formatVersion: 1
-      resources:
-        Cloud_vSphere_Network_1:
-          type: Cloud.vSphere.Network
-          properties:
-            networkType: existing
-            constraints:
-              - tag: 'networkCategory:Production'
-        Cloud_vSphere_Machine_1:
-          type: Cloud.vSphere.Machine
-          properties:
-            image: AK-Win2k16
-            flavor: AK-Medium
-            constraints:
-              - tag: 'cloud:on-prem'
-              - tag: 'cloud:vsphere'
-            networks:
-              - network: '$${resource.Cloud_vSphere_Network_1.id}'
-                assignment: dynamic
-    EOT
+  refresh_token = var.vra_refresh_token
 }
