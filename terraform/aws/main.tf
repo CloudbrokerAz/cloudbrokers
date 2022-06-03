418ce7f0bb6d76820fe8b96c66123c2e52907e04
aarone <aarone@outlook.com.au>
2022-06-03 15:50:37 +1000

    Create main.tf

---
+++ terraform/aws/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+resource "aws_instance" "myec2" {
+  ami           = "ami-0d2fb06f3c1484132"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Web Server"
+  }
+}
